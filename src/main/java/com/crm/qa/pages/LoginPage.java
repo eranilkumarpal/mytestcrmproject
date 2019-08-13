e32e14d9441913571fac7e87c00d70d31b58a25c
anil kumar pal <anilkumar.pal@gmail.com>
2019-08-13 14:31:09 +0530

    first git commit for my crm project

---
+++ src/main/java/com/crm/qa/pages/LoginPage.java
@@ -0,0 +1,57 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+public class LoginPage extends TestBase {
+
+	// Page factory :OR [Object Repository]
+
+	@FindBy(name = "username")
+	WebElement username;
+
+	@FindBy(name = "password")
+	WebElement password;
+
+	@FindBy(xpath = "//input[@type='submit']")
+	WebElement loginBtn;
+
+	@FindBy(xpath = "//a[contains(text(),'Sign Up')]")
+	WebElement signUpBtn;
+
+	@FindBy(xpath = "//a[@class='navbar-brand']//img[@class='img-responsive']")
+	WebElement crmLogo;
+
+	// Initialize the Page objects
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+		// PageFactory.initElements(driver, LoginPage.class);
+	}
+
+	// Actions:
+	public String validateLoginPageTitle() {
+		return driver.getTitle();
+
+	}
+
+	public boolean validateCRMImage() {
+		return crmLogo.isDisplayed();
+	}
+
+	public HomePage login(String un, String pw) {
+		
+		username.sendKeys(un);
+		password.sendKeys(pw);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		loginBtn.click();
+		return new HomePage();
+	}
+}
