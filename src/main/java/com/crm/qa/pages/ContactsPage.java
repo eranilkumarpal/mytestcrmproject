73aaa8a678b40bd007b8a0e80e58f5a1a7972e65
anil kumar pal <anilkumar.pal@gmail.com>
2019-08-13 14:57:24 +0530

    Contact page class change

--- src/main/java/com/crm/qa/pages/ContactsPage.java
+++ src/main/java/com/crm/qa/pages/ContactsPage.java
@@ -3,5 +3,10 @@ package com.crm.qa.pages;
 import com.crm.qa.base.TestBase;
 
 public class ContactsPage extends TestBase {
+	
+	public static void setup()
+	{
+		System.out.println("This is setup method");
+	}
 
 }
